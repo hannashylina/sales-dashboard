a01a1870c768a31f08c210849c1300952ea3a875
Hanna Shylina <hashy@ephyros.com>
2024-10-14 19:59:21 +0300

    Added shadows and data alignment for cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,9 +5,9 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex items-stretch flex-row-reverse justify-end">
-        <p class="p-4 grow">{{data.name}}</p>
-        <p class="p-4 flex-none">{{data.value}}</p>
+    <div class="flex items-stretch flex-row-reverse justify-end shadow">
+        <p class="p-4 grow flex items-center">{{data.name}}</p>
+        <p class="p-4 flex w-1/3 items-center justify-center">{{data.value}}</p>
     </div>
 </template>
 
