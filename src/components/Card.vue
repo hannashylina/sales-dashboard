5db5a9f099bbf5d95f12eeae9d5298250985fe8c
Hanna Shylina <hashy@ephyros.com>
2024-10-14 20:10:15 +0300

    Styles for cards values

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,9 +5,9 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex items-stretch flex-row-reverse justify-end shadow">
-        <p class="p-4 grow flex items-center">{{data.name}}</p>
-        <p class="p-4 flex w-1/3 items-center justify-center">{{data.value}}</p>
+    <div class="flex items-stretch flex-row justify-start shadow-md rounded-md">
+        <p class="p-4 flex w-1/3 items-center justify-center bg-emerald-600/75 text-white rounded-l-md">{{data.value}}</p>
+        <p class="py-4 px-6 grow flex items-center">{{data.name}}</p>
     </div>
 </template>
 
