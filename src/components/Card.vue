4e80832b83db69eb7e157878a53a444ffae4c148
Hanna Shylina <hashy@ephyros.com>
2024-10-14 21:13:41 +0300

    Changed layout for dashboard card to fit product name

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 
 <template>
     <div class="flex items-stretch flex-row justify-start shadow-md rounded-md">
-        <p class="p-4 flex w-1/3 items-center justify-center bg-emerald-600/75 text-white rounded-l-md">{{data.value}}</p>
+        <p class="py-4 px-6 flex whitespace-nowrap items-center justify-center bg-emerald-600/75 text-white rounded-l-md">{{data.value}}</p>
         <p class="py-4 px-6 grow flex items-center">{{data.name}}</p>
     </div>
 </template>
